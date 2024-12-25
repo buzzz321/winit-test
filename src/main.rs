8d76a2b3873b46661b3bcc365504a87c11a23285
Anders Olme <anders.olme@gmail.com>
2024-12-25 10:51:55 +0100

    Got init working!
    Tried to call and use window before it was used which was the issue of
    the panic. Now I inits more wgpu stuff but there is still much left.
    Also since some of the wgpu methods are async a async engine needed to
    be selected. Went with futures crate for now but might switch over to
    pollster later on.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use futures::executor;
 use winit::{
     application::ApplicationHandler,
     event::{ElementState, KeyEvent, WindowEvent},
@@ -6,22 +7,38 @@ use winit::{
     window::Window,
 };
 
+#[derive(Default, Debug)]
 struct App {
     window: Option<Window>,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self { window: Default::default() }
-    }
-}
-
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        self.window =
-            Some(event_loop
+        self.window = Some(
+            event_loop
                 .create_window(Window::default_attributes())
-                .unwrap());
+                .unwrap(),
+        );
+        println!("resumed -> Created new window!!");
+    }
+
+    fn new_events(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        cause: winit::event::StartCause,
+    ) {
+        match cause {
+            winit::event::StartCause::Init => {
+                self.window = Some(
+                    event_loop
+                        .create_window(Window::default_attributes())
+                        .unwrap(),
+                );
+                println!("new_events -> Created new window!!");
+                executor::block_on(self.init_wgpu());
+            }
+            _ => (),
+        }
     }
 
     fn window_event(
@@ -57,22 +74,59 @@ impl ApplicationHandler for App {
         }
     }
 }
-/* impl<'a> App<'a> {
-    pub fn new(window: & mut Window) -> Self {
-        let size = window.inner_size();
+impl App {
+    pub async fn init_wgpu(&mut self) {
+        let tmp = self.window.as_ref().unwrap();
+        let size = tmp.inner_size();
 
         let instance_descriptor = wgpu::InstanceDescriptor {
             backends: wgpu::Backends::all(),
             ..Default::default()
         };
         let instance = wgpu::Instance::new(instance_descriptor);
-        let surface = instance.create_surface(window).unwrap();
-        Self{
-            window: Some(window),
-        }
+        let surface = instance
+            .create_surface(self.window.as_ref().unwrap())
+            .unwrap();
+         let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+        let adapter = instance.request_adapter(&adapter_descriptor)
+            .await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+            memory_hints: wgpu::MemoryHints::Performance,
+            label: Some("Device"),
+        };
+                let (device, queue) = adapter
+            .request_device(&device_descriptor, None)
+            .await.unwrap();
+
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|f | f.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2
+        };
+        surface.configure(&device, &config);
     }
-   
-} */
+}
 fn main() {
     let event_loop = EventLoop::new().expect("Couldnt create event loop");
     event_loop.set_control_flow(ControlFlow::Poll);
